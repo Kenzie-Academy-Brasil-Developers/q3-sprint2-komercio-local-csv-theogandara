92735efa6593d2d7e4169b06b950f6eec0adba5b
Théo Gandara <theogandara@gmail.com>
2022-03-08 21:22:01 -0300

    feat: created routes delete,post,get,patch

--- app/__init__.py
+++ app/__init__.py
@@ -1,1 +1,98 @@
 # Suas rotas aqui
+from http import HTTPStatus
+from flask import Flask, jsonify, request
+from app.products.csv_products import read_products_from_csv
+from app.products.csv_products import add_products_in_csv
+from app.products.csv_products import rewrite_products_in_csv
+
+app = Flask(__name__)
+
+
+@app.get("/products")
+def home():
+    page = int(request.args.get("page", 1))
+    per_page = int(request.args.get("per_page", 3))
+
+    products = read_products_from_csv()
+
+    products_paginator = products[(page*per_page)-per_page:page*per_page]
+    return jsonify(products_paginator), HTTPStatus.OK
+
+
+@app.get("/products/<product_id>")
+def products(product_id):
+
+    products = read_products_from_csv()
+
+    result = []
+
+    for product in products:
+        if int(product_id) == product["id"]:
+            result.append(product)
+
+    if len(result) == 0:
+        return {"message": "ID not found"}
+
+    return result[0], HTTPStatus.OK
+
+
+@app.post("/products")
+def add_product():
+
+    data = request.get_json()
+    products = read_products_from_csv()
+
+    get_last_id = products[len(products) - 1]
+
+    product_id = {"id": int(get_last_id["id"]) + 1}
+    data.update(product_id)
+
+    product_price = {"price": float(data["price"])}
+    data.update(product_price)
+
+    add_products_in_csv(data)
+
+    return data, HTTPStatus.CREATED
+
+
+@app.delete("/products/<product_id>")
+def delete_product(product_id):
+
+    product_id = int(product_id)
+
+    products = read_products_from_csv()
+
+    product_to_be_deleted = [product for product in products if product["id"] == product_id]
+
+    new_list = [product for product in products if product["id"] != product_id]
+
+    rewrite_products_in_csv(new_list)
+
+    if len(product_to_be_deleted) == 0:
+        return {"message": "ID not found"}, HTTPStatus.NOT_FOUND
+
+    return product_to_be_deleted[0], HTTPStatus.OK
+
+
+@app.patch("/products/<product_id>")
+def update_product(product_id):
+    data = request.get_json()
+
+    product_price = {"price": float(data["price"])}
+    data.update(product_price)
+
+    product_id = int(product_id)
+
+    products = read_products_from_csv()
+
+    product_to_be_changed = []
+
+    for product in products:
+        if product_id == product["id"]:
+            product_to_be_changed.append(product)
+
+    if len(product_to_be_changed) == 0:
+        return {"message": "Product not found"}, HTTPStatus.NOT_FOUND
+
+    product_to_be_changed[0].update(data)
+    return (product_to_be_changed[0]), HTTPStatus.OK
---
+++ app/products/csv_products.py
@@ -0,0 +1,39 @@
+from csv import DictReader, DictWriter
+import os
+
+PATH = os.getenv("FILEPATH")
+
+
+def read_products_from_csv():
+    with open(PATH, "r") as csv_file:
+
+        reader = DictReader(csv_file)
+        products = list(reader)
+
+        for product in products:
+            for item in product:
+                if item == "price":
+                    new_price = {"price": float(product["price"])}
+                    product.update(new_price)
+                elif item == "id":
+                    new_id = {"id": int(product["id"])}
+                    product.update(new_id)
+
+        return products
+
+
+def add_products_in_csv(payload: dict):
+    with open(PATH, "a") as csv_file:
+        fieldnames = ["id", "name", "price"]
+        writer = DictWriter(csv_file, fieldnames)
+
+        writer.writerow(payload)
+
+
+def rewrite_products_in_csv(payload: list[dict]):
+    with open(PATH, "w") as csv_file:
+        fieldnames = ["id", "name", "price"]
+        writer = DictWriter(csv_file, fieldnames)
+
+        writer.writeheader()
+        writer.writerows(payload)
